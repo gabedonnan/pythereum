308c74f8ec226fb469ee91c9e9188034afd8e408
Gabriel Donnan <47415809+gabedonnan@users.noreply.github.com>
2023-07-25 22:20:39 +0100

    Added the beginnings of socket pooling

---
+++ socket_pool.py
@@ -0,0 +1,29 @@
+import asyncio
+import websockets
+
+class WebsocketPool:
+    def __init__(self, url: str, pool_size: int = 20):
+        self._url = url
+        self._id = 0
+        self._max_pool_size = pool_size
+        self._current_size = 0
+        self.sockets = asyncio.Queue()
+        self.connected = False
+
+    async def start(self):
+        if self.connected:
+            await self.quit()
+            self.connected = False
+        # Creates a number of sockets equal to the maximum pool size
+        connections = await asyncio.gather(self.open_socket() for _ in range(self._max_pool_size))
+        for connection in connections:
+            self.sockets.put_nowait(connection)
+
+    async def open_socket(self):
+        try:
+            yield websockets.connect(self._url)
+        finally:
+            ...
+
+    async def quit(self):
+        ...
