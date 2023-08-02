3d221fa9b91d7dcf0d6d6d5f68ed4816af809feb
gabedonnan <47415809+gabedonnan@users.noreply.github.com>
2023-08-02 23:47:31 +0100

    Updated readme to reflect changes

--- erpc_types.py
+++ erpc_types.py
@@ -1,6 +1,7 @@
 class Hex:
     def __init__(self, hex_string: str):
-        self.string = hex_string[2:] if hex_string[:2] == "0x" else hex_string
+        self.string = hex_string[2:] if hex_string.startswith(("0x", "0X")) else hex_string
+        self.binary = bin(int(self.string, 16))[2:]
         self.base = len(self.string)
 
     def __len__(self):
@@ -17,3 +18,8 @@ class Hex:
 
     def __repr__(self):
         return f"Hex(base={self.base}, string={self.string})"
+
+    def __bytes__(self):
+        return bytes(self.string, "utf-8")
+
+
