e7a2d0dd9dcd2c897beecd2b9cec323b9b8325d9
gabedonnan <47415809+gabedonnan@users.noreply.github.com>
2023-08-05 16:18:12 +0100

    Tests

--- t1.py
+++ t1.py
@@ -1,35 +1,49 @@
 import unittest
 import asyncio
+from time import time
 from eth_account import Account
 from eth_account.signers.local import LocalAccount
 
-from main import EthereumRPC, Block, EthereumRPCWebsocket
+from main import Block, EthRPC
 
 ANVIL_URL = "http://127.0.0.1:8545"
-TEST_URL = ""
-TEST_WS = ""
-erpc = EthereumRPC(TEST_URL)
-erpc_ws = EthereumRPCWebsocket(TEST_WS)
+erpc_ws = EthRPC(TEST_WS, 8)
+asyncio.run(erpc_ws.start_pool())
 
-
-async def test_block_num():
-    print("dx")
-    print(str(await erpc_ws.get_block_number()), "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee")
 
 class MyTestCase(unittest.IsolatedAsyncioTestCase):
+    async def test_pool_start(self):
+        await erpc_ws.start_pool()
+
     async def test_block_num(self):
-        r = await test_block_num()
-        self.assertTrue(r)
+        # await erpc_ws.start_pool()
+        t0 = time()
+        r1 = asyncio.create_task(erpc_ws.get_block_number())
+        r2 = asyncio.create_task(erpc_ws.get_transaction_count("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"))
+        r3 = asyncio.create_task(erpc_ws.get_balance("0xA69babEF1cA67A37Ffaf7a485DfFF3382056e78C"))
+        r4 = asyncio.create_task(erpc_ws.get_gas_price())
+        r5 = asyncio.create_task(erpc_ws.get_block_by_hash("0xdc0818cf78f21a8e70579cb46a43643f78291264dda342ae31049421c82d21ae", False))
+        r6 = asyncio.create_task(erpc_ws.get_block_by_number(17578346, False))
+
+        print(await r1)
+        print(await r2)
+        print(await r3)
+        print(await r4)
+        print(await r5)
+        print(await r6)
+        print(time() - t0)
+
+
 
     async def test_transaction_count(self):
         r = await erpc_ws.get_transaction_count("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
         self.assertIsInstance(r, int)
 
     async def test_wallet_balance(self):
-        self.assertIsInstance(erpc_ws.get_balance("0xA69babEF1cA67A37Ffaf7a485DfFF3382056e78C"), int)
+        self.assertIsInstance(await erpc_ws.get_balance("0xA69babEF1cA67A37Ffaf7a485DfFF3382056e78C"), int)
 
     async def test_gas_price(self):
-        self.assertIsInstance(erpc_ws.get_gas_price(), int)
+        self.assertIsInstance(await erpc_ws.get_gas_price(), int)
 
     async def test_get_block_by_hash(self):
         r = await erpc_ws.get_block_by_hash("0xdc0818cf78f21a8e70579cb46a43643f78291264dda342ae31049421c82d21ae", False)
@@ -39,13 +53,13 @@ class MyTestCase(unittest.IsolatedAsyncioTestCase):
         r = await erpc_ws.get_block_by_number(17578346, False)
         print(r)
 
-    def test_eth_call(self):
+    async def test_eth_call(self):
         tx = {"from" : "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
               "to": "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
               "value": hex(1),
               "gas": hex(38983301337),
               "type": hex(1)}
-        print(erpc_ws.call(tx))
+        print(await erpc_ws.call(tx))
 
     def test_send_transaction(self):
         tx = {"from": "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
