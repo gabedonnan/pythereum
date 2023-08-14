d148bdd75fd197934c05566e0a306c4ed92fdece
Gabriel Donnan <47415809+gabedonnan@users.noreply.github.com>
2023-08-14 22:07:57 +0100

    Added comments

--- demo/listen_blocks.py
+++ demo/listen_blocks.py
@@ -5,13 +5,22 @@ from dotenv import dotenv_values
 config = dotenv_values("../.env")  # Pulls variables from .env into a dictionary
 
 async def listen_blocks(url):
-    erpc = EthRPC(url, 4)
+    # Create EthRPC object with pool size of 2 (arbitrarily chosen, as it does not matter here)
+    erpc = EthRPC(url, 2)
+    
+    # Start the socket pool, may take a while due to connection forming
     await erpc.start_pool()
+
+    # Create + context manage new_heads subscription
     async with erpc.subscribe(SubscriptionType.new_heads) as sc:
+        # Loops forever over the received data from the subscription
         async for header in sc.recv():
-            # print(header.hash.hex_string)
-            block = await erpc.get_block_by_hash(header.hash.hex_string, True)
+            # Gets more block data from the hash received from the headers
+            block = await erpc.get_block_by_hash(header.hash, True)
+
+            # Iterates through the transactions found in retrieved data
             for tx in block.transactions:
+                # Gets and prints the receipts for each transaction
                 r = await erpc.get_transaction_receipt(tx.hash)
                 print(r)
     await erpc.close()
