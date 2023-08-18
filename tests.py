ce145d2b7234733647d6824b2c199f6ebfb7ecf4
gmandonnan <gabedonnan@hotmail.co.uk>
2023-08-18 16:33:13 +0100

    Added test for multiple simultaneous batch calls

--- tests.py
+++ tests.py
@@ -40,6 +40,41 @@ class MyTestCase(unittest.IsolatedAsyncioTestCase):
             r6 = tg.create_task(erpc_ws.get_block_by_number(17578346, False))
         print(time() - t0)
 
+    async def test_batch_tasks(self):
+        await self.erpc_ws.start_pool()
+        t0 = time()
+        async with asyncio.TaskGroup() as tg:
+            t3 = tg.create_task(self.erpc_ws.get_block_by_number(
+                [i for i in range(1000,1010)],
+                full_object=[False for _ in range(10)]
+            ))
+
+            t4 = tg.create_task(self.erpc_ws.get_block_by_number(
+                [i for i in range(3000, 3010)],
+                full_object=[False for _ in range(10)]
+            ))
+
+            t5 = tg.create_task(self.erpc_ws.get_block_by_number(
+                [i for i in range(6000, 6010)],
+                full_object=[False for _ in range(10)]
+            ))
+
+            t6 = tg.create_task(self.erpc_ws.get_block_by_number(
+                [i for i in range(6010, 6020)],
+                full_object=[False for _ in range(10)]
+            ))
+
+            t7 = tg.create_task(self.erpc_ws.get_block_by_number(
+                [i for i in range(6020, 6030)],
+                full_object=[False for _ in range(10)]
+            ))
+
+            t8 = tg.create_task(self.erpc_ws.get_block_by_number(
+                [i for i in range(6030, 6040)],
+                full_object=[False for _ in range(10)]
+            ))
+        print(time() - t0)
+
     async def test_transaction_count(self):
         r = await self.erpc_ws.get_transaction_count("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
         self.assertIsInstance(r, int)
