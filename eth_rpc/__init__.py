ebbf58646870c1a9a4aeba9a156dd1613b3795fb
gmandonnan <gabedonnan@hotmail.co.uk>
2023-08-22 21:48:01 +0100

    Updated documentation to represent new name

--- eth_rpc/__init__.py
+++
@@ -1,3 +0,0 @@
-from eth_rpc.rpc import EthRPC, SubscriptionType, BlockTag, CurrencyValue, convert_eth
-from eth_rpc.common import Hex
-from eth_rpc.dclasses import Sync, Receipt, Block, Log, Transaction, Access
