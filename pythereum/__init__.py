84c59f6b67945a310cba3367a5f99e8cba777094
gmandonnan <gabedonnan@hotmail.co.uk>
2023-09-03 21:48:26 +0100

    Added new transaction type to __init__

--- pythereum/__init__.py
+++ pythereum/__init__.py
@@ -1,3 +1,3 @@
 from pythereum.rpc import EthRPC, SubscriptionType, BlockTag, EthDenomination, convert_eth
 from pythereum.common import HexStr
-from pythereum.dclasses import Sync, Receipt, Block, Log, TransactionFull, Access
+from pythereum.dclasses import Sync, Receipt, Block, Log, TransactionFull, Access, Transaction
