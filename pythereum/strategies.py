89543a7ff5d62ee4b7437d5aa174b26fbecaa01d
gmandonnan <gabedonnan@hotmail.co.uk>
2023-09-21 14:32:31 +0100

    Restructured Gas management strategy

--- pythereum/strategies.py
+++ pythereum/strategies.py
@@ -1,75 +1,90 @@
-import asyncio
 import statistics
 
 from math import inf
 from time import time
+from enum import Enum
 from pythereum.rpc import EthRPC, BlockTag
-from pythereum.dclasses import Receipt
+from pythereum.dclasses import TransactionFull, Transaction
 from pythereum.exceptions import ERPCInvalidReturnException
 
 # auto get nonce for user, automatically supply gas stuff, etc.
 
 
-class GasStrategy:
+class GasStrategy(Enum):
+    min_price = 0
+    max_price = 1
+    median_price = 2
+    mean_price = 3
+    mode_price = 4
+    upper_quartile_price = 5
+    lower_quartile_price = 6
+
+
+class GasManager:
     def __init__(self, rpc: EthRPC):
         self.rpc = rpc
-        self.latest_receipts = None
+        self.latest_transactions = None
         self.storage_time = None  # The time at which the most recent storage action was taken
 
-    async def _get_latest_receipts(self, use_stored_results: bool) -> tuple[Receipt]:
+    async def _get_latest_receipts(self, use_stored_results: bool) -> list[TransactionFull]:
         """
         Returns a tuple of the latest transaction receipts.
         These are gotten by getting the latest block info and requesting transaction receipts for each transaction.
         To avoid doing this for every call, there is the option to use stored results from the most recent request.
         """
         if use_stored_results:
-            transaction_receipts = self.latest_receipts
+            transactions = self.latest_transactions
         else:
-            latest_block = await self.rpc.get_block_by_number(BlockTag.latest)
-            transaction_receipts = await asyncio.gather(
-                *(self.rpc.get_transaction_receipt(transaction) for transaction in latest_block.transactions)
-            )
-            self.latest_receipts = transaction_receipts
+            latest_block = await self.rpc.get_block_by_number(BlockTag.latest, True)
+            transactions = latest_block.transactions
+            self.latest_transactions = transactions
             self.storage_time = time()
-        if len(transaction_receipts) == 0:
-            raise ERPCInvalidReturnException(f"Invalid vlue: {transaction_receipts} returned from _get_latest_receipts")
-        return transaction_receipts
+        if len(transactions) == 0:
+            raise ERPCInvalidReturnException(f"Invalid vlue: {transactions} returned from _get_latest_receipts")
+        return transactions
 
-    async def min_price(self, attribute: str = "effective_gas_price", use_stored_results: bool = False) -> int:
-        transaction_receipts = await self._get_latest_receipts(use_stored_results)
-        return min([x.__getattribute__(attribute) for x in transaction_receipts])
+    async def suggest(
+        self,
+        strategy: GasStrategy = GasStrategy.mean_price,
+        attribute: str = "gas",
+        use_stored_results: bool = False
+    ) -> float:
+        transactions = await self._get_latest_receipts(use_stored_results)
+        prices = [x.__getattribute__(attribute) for x in transactions]
+        match strategy:
+            case GasStrategy.min_price:
+                return min(prices)
+            case GasStrategy.max_price:
+                return max(prices)
+            case GasStrategy.median_price:
+                return statistics.median(prices)
+            case GasStrategy.mean_price:
+                return statistics.mean(prices)
+            case GasStrategy.upper_quartile_price:
+                return statistics.quantiles(prices, n=4)[2]
+            case GasStrategy.lower_quartile_price:
+                return statistics.quantiles(prices, n=4)[0]
 
-    async def max_price(self, attribute: str = "effective_gas_price", use_stored_results: bool = False) -> int:
-        transaction_receipts = await self._get_latest_receipts(use_stored_results)
-        return max([x.__getattribute__(attribute) for x in transaction_receipts])
-
-    async def median_price(self, attribute: str = "effective_gas_price", use_stored_results: bool = False) -> float:
-        transaction_receipts = await self._get_latest_receipts(use_stored_results)
-        return statistics.median([x.__getattribute__(attribute) for x in transaction_receipts])
-
-    async def mean_price(self, attribute: str = "effective_gas_price", use_stored_results: bool = False) -> float:
-        transaction_receipts = await self._get_latest_receipts(use_stored_results)
-        return statistics.mean([x.__getattribute__(attribute) for x in transaction_receipts])
-
-    async def mode_price(self, attribute: str = "effective_gas_price", use_stored_results: bool = False) -> int:
-        transaction_receipts = await self._get_latest_receipts(use_stored_results)
-        return statistics.mode([x.__getattribute__(attribute) for x in transaction_receipts])
-
-    async def upper_quartile_price(self, attribute: str = "effective_gas_price", use_stored_results: bool = False) -> float:
-        transaction_receipts = await self._get_latest_receipts(use_stored_results)
-        return statistics.quantiles([x.__getattribute__(attribute) for x in transaction_receipts], n=4)[2]
-
-    async def lower_quartile_price(self, attribute: str = "effective_gas_price", use_stored_results: bool = False) -> float:
-        transaction_receipts = await self._get_latest_receipts(use_stored_results)
-        return statistics.quantiles([x.__getattribute__(attribute) for x in transaction_receipts], n=4)[0]
-
-    async def percentile_price(self, percentile: int, attribute: str = "effective_gas_price", use_stored_results: bool = False) -> float:  # noqa
-        """
-        Gets the lower nth percentile, where 0 will be the lowest value and 98 will be the highest
-        """
-        percentile = min(max(percentile, 0), 98)
-        transaction_receipts = await self._get_latest_receipts(use_stored_results)
-        return statistics.quantiles([x.__getattribute__(attribute) for x in transaction_receipts], n=100)[percentile]
+    async def fill_transaction(
+        self,
+        tx: Transaction | list[Transaction],
+        strategy: GasStrategy = GasStrategy.mean_price | dict[str, GasStrategy]
+    ) -> None:
+        if isinstance(strategy, GasStrategy):  # Allows for separation of strategy types for each type
+            strategy = {"gas": strategy, "maxFeePerGas": strategy, "maxPriorityFeePerGas": strategy}
+        if isinstance(tx, list):
+            for sub_tx in tx:
+                sub_tx["gas"] = await self.suggest(strategy["gas"], "gas", False)
+                sub_tx["maxFeePerGas"] = await self.suggest(strategy["maxFeePerGas"], "maxFeePerGas", True)
+                sub_tx["maxPriorityFeePerGas"] = await self.suggest(
+                    strategy["maxPriorityFeePerGas"], "maxPriorityFeePerGas", True
+                )
+        elif tx is not None:
+            tx["gas"] = await self.suggest(strategy["gas"], "gas", False)
+            tx["maxFeePerGas"] = await self.suggest(strategy["maxFeePerGas"], "maxFeePerGas", True)
+            tx["maxPriorityFeePerGas"] = await self.suggest(
+                strategy["maxPriorityFeePerGas"], "maxPriorityFeePerGas", True
+            )
 
     @property
     def storage_age(self):
@@ -80,6 +95,3 @@ class GasStrategy:
         if self.storage_time is None:
             return inf
         return time() - self.storage_time
-
-
-
